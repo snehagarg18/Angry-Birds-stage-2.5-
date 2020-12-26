99a4b8d6afd689cfab3a416deee1c1b5223544ee
snehagarg18 <snehagarg170@gmail.com>
2020-12-26 13:43:13 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg;
+var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
